20d8a813f466f87dc9060e0c1b20e8d48a32d094
groys <gautam.roy@gmail.com>
2021-12-11 22:51:19 -0800

    day 12

---
+++ 2021/day12.rb
@@ -0,0 +1,88 @@
+# BFS
+# keep track of small visited
+# if you hit end
+#
+# rep graph
+# h = map <node, list<node>>
+require 'set'
+
+def small(n)
+  n == n.downcase
+end
+
+def visit(n)
+  if n == "end"
+    #puts "$v = #{$v}"
+    $path << "end"
+    $paths << $path.clone
+    $path.delete("end")
+    return
+  end
+  if small(n)
+    if $v.keys.include?(n)
+      unless n == $twice && $v[n] == 1
+        return
+      end
+    end
+    if n == $twice
+      if $v[n].nil?
+        $v[n] = 1
+      else
+        $v[n] = 2
+      end
+    else
+      $v[n] = 1
+    end
+  end
+  $path << n
+  # puts "v = #{$v}"
+  # puts "pat = #{$path}"
+  unless $h[n].nil?
+    # puts "n = #{n} adj = #{$h[n]}"
+    $h[n].each { |ne|
+      visit(ne)
+    }
+  end
+  if n == $twice
+    if $v[n] == 2
+      $v[n] = 1
+    else
+      $v.delete(n)
+    end
+  else
+    $v.delete(n)
+  end
+  $path.pop
+  # puts "v = #{$v}"
+end
+
+$h = {}
+while l = gets
+  a, b = l.strip!.split("-")
+  if $h[a].nil?
+    $h[a] = []
+  end
+  $h[a] << b
+  if $h[b].nil?
+    $h[b] = []
+  end
+  $h[b] << a
+end
+
+puts "#{$h}"
+$smalls = $h.keys.filter{|k| k.downcase == k && k != "start" && k != "end"}
+puts "#{$smalls}"
+$paths = Set.new
+$path = []
+$smalls.each do |s|
+  $twice = s
+  $v = {}
+  visit("start")
+end
+# $paths.each do |p|
+#   puts "#{p}"
+# end
+puts "#{$paths.size}"
+
+
+
