cce141c5ddc1232659b68c21d7bc20fbb55268ee
groys <gautam.roy@keeptruckin.com>
2021-12-18 15:07:58 -0800

    day 18

--- 2021/day18.rb
+++ 2021/day18.rb
@@ -5,8 +5,47 @@
 # To change this template use File | Settings | File Templates.
 
 require 'json'
+# require 'test/unit/assertions'
+# include Test::Unit::Assertions
 # add one by one
 # find magnitude of final recursively
+
+def test_single_reduce(s1, s2)
+  s, st = reduce(s1)
+  assert_equal(s, s2)
+end
+
+def test_explode
+  s1 = "[[[[[9,8],1],2],3],4]"
+  s2 = "[[[[0,9],2],3],4]"
+  test_single_reduce(s1, s2)
+  test_single_reduce("[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]")
+  test_single_reduce("[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]")
+  test_single_reduce("[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]")
+  test_single_reduce("[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]")
+end
+
+def test_add_2
+  s1 = "[[[[4,3],4],4],[7,[[8,4],9]]]"
+  s2 = "[1,1]"
+  res = "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"
+  assert_equal(add([s1, s2]), res)
+end
+
+def test_add
+  res = "[[[[1,1],[2,2]],[3,3]],[4,4]]"
+  lines = ["[1,1]", "[2,2]", "[3,3]", "[4,4]"]
+  assert_equal(add(lines), res)
+  res = "[[[[3,0],[5,3]],[4,4]],[5,5]]"
+  lines = ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]"]
+  assert_equal(add(lines), res)
+  res = "[[[[5,0],[7,4]],[5,5]],[6,6]]"
+  lines = ["[1,1]", "[2,2]", "[3,3]", "[4,4]", "[5,5]", "[6,6]"]
+  assert_equal(add(lines), res)
+  res = "[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"
+  lines = ["[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]", "[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]"]
+  assert_equal(add(lines), res)
+end
 
 def is_num(e)
   Integer(e || '')
@@ -15,7 +54,7 @@ rescue ArgumentError
 end
 
 def add_left(arr, i, num)
-  puts "al i = #{i}, n = #{num}"
+  # puts "al i = #{i}, n = #{num}"
   while i >= 0
     left = is_num(arr[i])
     if !left.nil?
@@ -29,7 +68,7 @@ def add_left(arr, i, num)
 end
 
 def add_right(arr, i, num)
-  puts "ar i = #{i}, n = #{num}"
+  # puts "ar i = #{i}, n = #{num}"
   while i < arr.length
     right = is_num(arr[i])
     if !right.nil?
@@ -42,15 +81,25 @@ def add_right(arr, i, num)
   false
 end
 
-def reduce(arr, st, ns)
-  puts "reduce arr = #{arr.join} st = #{st} ns = #{ns}"
-  sleep(1)
-  n = ns
-  for i in st...arr.length do
+def reduce(a)
+  arr = a.split(/([\[,\,,\]])/).select { |x| !x.empty? }
+  # puts "arr = #{arr}"
+  # puts "reduce a = #{a}"
+  if explode(arr)
+    return [arr.join, true]
+  end
+  if split(arr)
+    return [arr.join, true]
+  end
+  [arr.join, false]
+end
+
+def explode(arr)
+  n = 0
+  for i in 0...arr.length do
     e = arr[i]
     if e == "["
       n += 1
-
       next
     end
     if e == "]"
@@ -61,11 +110,15 @@ def reduce(arr, st, ns)
       next
     end
     num = e.to_i
+    if num.nil?
+      puts "ERRR"
+      exit
+    end
     # is it regular pair?
     if i < arr.length - 2
       r = is_num(arr[i + 2])
       if arr[i + 1] == "," && !r.nil? && n > 4
-        puts "boom n = #{n}"
+        # puts "boom n = #{n}"
         #find left
         l = add_left(arr, i - 1, num)
         # find right
@@ -78,14 +131,20 @@ def reduce(arr, st, ns)
         arr.delete_at(i - 1)
         arr.insert(i - 1, 0)
         if l || r
-          reduce(arr, st, ns)
-        else
-          reduce(arr, i, n)
+          return true
         end
       end
     end
-    if num >= 10
-      puts "split"
+  end
+  false
+end
+
+def split(arr)
+  for i in 0...arr.length do
+    e = arr[i]
+    num = e.to_i
+    if !num.nil? && num >= 10
+      # puts "split"
       arr.delete_at(i)
       arr.insert(i, "[")
       arr.insert(i + 1, (num / 2.0).floor)
@@ -93,33 +152,74 @@ def reduce(arr, st, ns)
 
       arr.insert(i + 3, (num / 2.0).ceil)
       arr.insert(i + 4, "]")
-      reduce(arr, st, ns)
+      return true
     end
   end
+  false
 end
 
-def magnitude
-
-end
-
-a = nil
-while l = gets
-  l.strip!
-  if a.nil?
-    a = JSON.parse(l)
-    puts "a1 = #{a}"
+def magnitude(a)
+  # puts "ma = #{a}"
+  inp = JSON.parse(a)
+  if inp.is_a? Integer
+    inp
   else
-    a = [a, JSON.parse(l)]
-    puts "a2 = #{a}"
-    s = a.to_json
-    puts "s1 = #{s}"
-    arr = s.split(/([\[,\,,\]])/).select { |x| !x.empty? }
-    puts "arr = #{arr}"
-    reduce(arr, 0, 0)
-    s = arr.join
-    puts "s = #{s}"
-    a = JSON.parse(s)
-    puts "a = #{a}"
+    3 * magnitude(inp[0].to_s) + 2 * magnitude(inp[1].to_s)
   end
+
 end
-puts "#{a}"
+
+def add(lines)
+  a = nil
+  lines.each do |l|
+    if a.nil?
+      a = l
+    else
+      a = "[" + a + "," + l + "]"
+      res = true
+      while res
+        a, res = reduce(a)
+        # puts "a = #{a}"
+      end
+    end
+  end
+  a
+end
+
+def readin
+  lines = []
+  while l = gets
+    l.strip!
+    lines << l
+  end
+  lines
+end
+
+def find_max_magnitude(lines)
+  max = -1
+  for i in 0...lines.size
+    for j in (i + 1)...lines.size
+      # puts "#{i} #{j}"
+      a = add([lines[i], lines[j]])
+      m = magnitude(a)
+      if max < m
+        max = m
+      end
+    end
+  end
+  max
+end
+
+def part1(lines)
+  m = magnitude(add(lines))
+  puts "p1 = #{m}"
+end
+
+def part2(lines)
+  puts "p2 = #{find_max_magnitude(lines)}"
+end
+
+lines = readin
+part1(lines)
+part2(lines)
+
