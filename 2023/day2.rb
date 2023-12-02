e0bba0957e5573238d61ea8e185a17753833626d
groys <gautam.roy@gmail.com>
2023-12-01 22:08:55 -0800

    day2 part2

--- 2023/day2.rb
+++ 2023/day2.rb
@@ -1,31 +1,33 @@
 $stdout.sync = true
+
 g = 1
-rmax = 12
-gmax = 13
-bmax = 14
 res = 0
 while line = gets
   arr = line.split(':')
   line = arr[1]
   arr = line.split(';')
   # arr = [" 3 blue, 4 red", " 1 red, 2 green, 6 blue", " 2 green"]
-  valid = true
+  rm = -1
+  gm = -1
+  bm = -1
   for s in arr
     # s = " 3 blue, 4 red"
     for token in s.split(',')
       ns, w = token.split
-      if (w.start_with?('b') && ns.to_i > bmax) ||  (w.start_with?('g') && ns.to_i > gmax) ||  (w.start_with?('r') && ns.to_i > rmax)
-        valid = false
-        break
+      n = ns.to_i
+      if (w.start_with?('b') && n > bm)
+        bm = n
+      end
+      if (w.start_with?('g') && n > gm)
+        gm = n
+      end
+      if (w.start_with?('r') && n > rm)
+        rm = n
       end
     end
-    if !valid
-      break
-    end
   end
-  if valid
-    res += g
-  end
+  res += (rm * gm * bm)
   g += 1
 end
 puts res
+
