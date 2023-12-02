2cd8ce92438f540fcaf55c04513284c1df6d27b9
groys <gautam.roy@gmail.com>
2023-12-02 00:12:39 -0800

    fix up fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -4,8 +4,7 @@
 
 set -euo pipefail
 
-BASEDIR=$(dirname "$0")
 YEAR=$1; shift
 DAY=$1; shift
-COOKIE=$(cat $BASEDIR/.aoc-session-cookie)
-curl -o  "$BASEDIR/2021/input$DAY.txt" -L https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$COOKIE"
+COOKIE=$(cat .aoc-session-cookie)
+curl -o  "$YEAR/input$DAY.txt" -L https://adventofcode.com/$YEAR/day/$DAY/input --cookie "session=$COOKIE"
