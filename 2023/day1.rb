48c046e58707c20db06a21c48c383de0d56f5199
groys <gautam.roy@gmail.com>
2023-12-01 23:25:18 -0800

    day1 2023

--- 2023/day1.rb
+++ 2023/day1.rb
@@ -1,5 +1,4 @@
 
-
 def getNum(line)
   #puts line
   f = ''
@@ -22,10 +21,45 @@ def getNum(line)
   end
   10 * (f.to_i) + l.to_i
 end
-nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
+def getNumwithWord(line)
+  nums = {"one": "1", "two": "2", "three":"3", "four":"4", "five":"5", "six":"6", "seven":"7", "eight":"8", "nine":"9"}
+  firstNum = -1
+  lastNum = -1
+  firstIdx = line.length
+  lastIdx = -1
+  nums.each do |k,v|
+    ks = k.to_s
+
+    id = line.index(ks)
+    if !id.nil? && id < firstIdx
+      firstIdx = id
+      firstNum = v
+    end
+    id = line.index(v)
+    if !id.nil? && id < firstIdx
+      firstIdx = id
+      firstNum = v
+    end
+
+    rid = line.rindex(ks)
+    if !rid.nil? && rid > lastIdx
+      lastIdx = rid
+      lastNum = v
+    end
+    rid = line.rindex(v)
+    if !rid.nil? && rid > lastIdx
+      lastIdx = rid
+      lastNum = v
+    end
+  end
+  10 * (firstNum.to_i) + lastNum.to_i
+end
+
 $stdout.sync = true
 res = 0
 while line = gets
-  res += getNum(line)
+  puts line
+  res += getNumwithWord(line)
 end
 puts res
